cb988dc41f6e88c6fcd07b199f785fe22560ff55
naneen <nazneenbanosiddiqui@gmail.com>
2025-02-23 21:11:22 +0530

    dcf

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "./css/main.css"
+import "./css/style.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Store from './redux/Store.jsx';
 import { Provider } from 'react-redux';
